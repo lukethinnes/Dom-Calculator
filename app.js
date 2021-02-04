84513f25ddf1b936affb477bb47d93ec8f752019
Luke Thinnes <lukethinnes@yahoo.com>
2021-02-04 11:00:10 -0700

    Progress by 11:00.

---
+++ app.js
@@ -0,0 +1,87 @@
+const screen = document.querySelector('#screen')
+
+const add = document.querySelector('#add')
+const subtract = document.querySelector('#subtract')
+const multiply = document.querySelector('#multiply')
+const divide = document.querySelector('#divide')
+
+const zero = document.querySelector('#zero')
+const one = document.querySelector('#one')
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const nine = document.querySelector('#nine')
+
+add.addEventListener('click', event => {
+    const number = add.textContent
+    screen.append(number)
+})
+
+subtract.addEventListener('click', event => {
+    const number = subtract.textContent
+    screen.append(number)
+})
+
+multiply.addEventListener('click', event => {
+    const number = multiply.textContent
+    screen.append(number)
+})
+
+divide.addEventListener('click', event => {
+    const number = divide.textContent
+    screen.append(number)
+})
+
+zero.addEventListener('click', event => {
+    const number = zero.textContent
+    screen.append(number)
+})
+
+one.addEventListener('click', event => {
+    const number = one.textContent
+    screen.append(number)
+})
+
+two.addEventListener('click', event => {
+    const number = two.textContent
+    screen.append(number)
+})
+
+three.addEventListener('click', event => {
+    const number = three.textContent
+    screen.append(number)
+})
+
+four.addEventListener('click', event => {
+    const number = four.textContent
+    screen.append(number)
+})
+
+five.addEventListener('click', event => {
+    const number = five.textContent
+    screen.append(number)
+})
+
+six.addEventListener('click', event => {
+    const number = six.textContent
+    screen.append(number)
+})
+
+seven.addEventListener('click', event => {
+    const number = seven.textContent
+    screen.append(number)
+})
+
+eight.addEventListener('click', event => {
+    const number = eight.textContent
+    screen.append(number)
+})
+
+nine.addEventListener('click', event => {
+    const number = nine.textContent
+    screen.append(number)
+})
